710ccc587662c5c3f35ec520ff3650a1e227e138
Cristian <cregueiromartinez@danielcastelao.org>
2024-10-10 11:00:44 +0200

    añadido try-catch

--- src/IngresosClientes.java
+++ src/IngresosClientes.java
@@ -3,14 +3,22 @@ import javax.swing.*;
 public class IngresosClientes implements Runnable{
 
     CajaEmpresa ce = new CajaEmpresa();
+    private int ingreso;
 
     @Override
     public void run() {
 
         for(int i = 0; i < 10; i++){
 
-            int ingreso = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el dinero para la compra del artículo, por favor: "));
-            
+            do{
+                ingreso = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el dinero para la compra del artículo, por favor: "));
+                try{
+
+                }catch(NumberFormatException e){
+                    ingreso = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el dinero para la compra del artículo, por favor: "));
+                }
+            }while(ingreso <=0);
+
 
         }
 
